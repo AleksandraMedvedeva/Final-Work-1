0b2a79579a771219fe174adfaf7df7b62033894c
Aleksandra Medvedeva <alexa-nt@mail.ru>
2024-05-30 15:17:47 +0500

    Добавлен код для ввода количества элементов массива и их значений с консоли

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введити количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элементы массива через пробел:");
+string[] array1 = Console.ReadLine().Split(' ');
