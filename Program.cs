44f7c9994e8187d5d9ee06b141dae6b8b54f9eb4
Aleksandra Medvedeva <alexa-nt@mail.ru>
2024-06-04 15:40:46 +0500

    Добавлен метод для формирования нового массива

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите элементы массива через пробел:");
 string[] array1 = Console.ReadLine().Split(' ');
 string[] array2 = new string[array1.Length];
+
 void SecondArray(string[] array1, string[] array2)
 {
     int j = 0; for (int i = 0; i < array1.Length; i++)
